6fbdcbfa81edfb69eae3ebe03be0fed98040f5ae
lily102904 <tJk9Gic3Lib6a86>
2024-12-14 22:20:11 +0000

    change

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -4,4 +4,4 @@ import axios from 'axios';
 //     process.env.NODE_ENV !== 'production'
 //         ? 'http://localhost:5000'
 //         : process.env.REACT_APP_API_BASE_URL;
-axios.defaults.baseURL = 'https://canteen-ordering-system-backend.onrender.com/api';
+axios.defaults.baseURL = 'https://canteen-ordering-system-backend.onrender.com';
