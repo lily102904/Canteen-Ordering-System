e637ece5bfce41226c6f3bc334814c90592f2057
lily102904 <134790778+lily102904@users.noreply.github.com>
2024-12-14 21:13:54 +0000

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000'
+    : process.env.REACT_APP_API_BASE_URL;
